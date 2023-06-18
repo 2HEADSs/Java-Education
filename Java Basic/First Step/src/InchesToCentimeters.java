eb3a266106649f1c6d1ac3a08d9040d85cc41f07
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-06-18 10:52:04 +0300

    Update InchesToCentimeters.java

--- Java Basic/First Step/src/InchesToCentimeters.java
+++ Java Basic/First Step/src/InchesToCentimeters.java
@@ -4,9 +4,9 @@ public class InchesToCentimeters {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int inputNum = Integer.parseInt(scanner.nextLine());
+        double inputInch = Double.parseDouble(scanner.nextLine());
 
-        double result = inputNum * 2.54;
-        System.out.println(result);
+        double cm = inputInch * 2.54;
+        System.out.println(cm);
     }
 }
