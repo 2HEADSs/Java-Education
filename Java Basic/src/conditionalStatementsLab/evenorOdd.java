a1173d8e6f386533b8d2539f8807892488770cc4
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-06-19 16:00:46 +0300

    Update evenorOdd.java

--- Java Basic/src/conditionalStatementsLab/evenorOdd.java
+++ Java Basic/src/conditionalStatementsLab/evenorOdd.java
@@ -1,7 +1,14 @@
 package conditionalStatementsLab;
 
+import java.util.Scanner;
+
 public class evenorOdd {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = Integer.parseInt(scanner.nextLine());
+
+
 
     }
 }
