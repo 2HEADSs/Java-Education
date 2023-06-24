aa925ffa734af6ac065f81cdf5c87fb08380a1cd
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-06-24 15:13:59 +0300

    Update weekendorWorkingDay.java

--- Java Basic/src/conditionalStatementsAdvancedLab/weekendorWorkingDay.java
+++ Java Basic/src/conditionalStatementsAdvancedLab/weekendorWorkingDay.java
@@ -1,4 +1,28 @@
 package conditionalStatementsAdvancedLab;
 
+import java.util.Scanner;
+
 public class weekendorWorkingDay {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String dayOFWeek = scanner.nextLine();
+
+        switch (dayOFWeek){
+            case "Monday":
+            case "Tuesday":
+            case "Wednesday":
+            case "Thursday":
+            case "Friday":
+                System.out.println("Working Day");
+                break;
+            case "Saturday":
+            case "Sunday":
+                System.out.println("Weekend");
+                break;
+            default:
+                System.out.println("Error");
+                break;
+        }
+    }
 }
