d5532585dd09463fc3201f30218cb29b79958040
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-06-24 15:51:59 +0300

    Update personalTitles.java

--- Java Basic/src/conditionalStatementsAdvancedLab/personalTitles.java
+++ Java Basic/src/conditionalStatementsAdvancedLab/personalTitles.java
@@ -1,4 +1,29 @@
 package conditionalStatementsAdvancedLab;
 
+import java.util.Scanner;
+
 public class personalTitles {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String gender = scanner.nextLine();
+        double age = Double.parseDouble(scanner.nextLine());
+
+        if(gender.equals("m")){
+            if(age >=16){
+                System.out.println("Mr.");
+
+            } else  if(age < 16){
+                System.out.println("Master");
+            }
+        } else  if(gender.equals("f")){
+            if(age >=16){
+                System.out.println("Ms.");
+
+            } else  if(age < 16){
+                System.out.println("Miss");
+            }
+        }
+
+    }
 }
