f86c5c972737e59761a069d85972b226147db4f8
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-06-24 15:36:42 +0300

    Update fruitOrVegetable.java

--- Java Basic/src/conditionalStatementsAdvancedLab/fruitOrVegetable.java
+++ Java Basic/src/conditionalStatementsAdvancedLab/fruitOrVegetable.java
@@ -1,4 +1,35 @@
 package conditionalStatementsAdvancedLab;
 
+import java.util.Scanner;
+
+//•	Плодовете "fruit" са banana, apple, kiwi, cherry, lemon и grapes
+//        •	Зеленчуците "vegetable" са tomato, cucumber, pepper и carrot
+//        •	Всички останали са "unknown"
+
 public class fruitOrVegetable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String fruit = scanner.nextLine();
+
+        switch (fruit){
+            case "banana":
+            case "apple":
+            case "kiwi":
+            case "cherry":
+            case "lemon":
+            case "grapes":
+                System.out.println("fruit");
+                break;
+            case "tomato":
+            case "cucumber":
+            case "carrot":
+            case "pepper":
+                System.out.println("vegetable");
+                break;
+            default:
+                System.out.println("unknown");
+                break;
+        }
+    }
 }
