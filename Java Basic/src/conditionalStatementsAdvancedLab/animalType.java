33910712745250f37eebf5079fee9f8b7e213c86
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-06-24 15:17:44 +0300

    Create animalType.java

---
+++ Java Basic/src/conditionalStatementsAdvancedLab/animalType.java
@@ -0,0 +1,10 @@
+package conditionalStatementsAdvancedLab;
+//•	dog -> mammal
+//        •	crocodile, tortoise, snake -> reptile
+//        •	others -> unknown
+
+public class animalType {
+    public static void main(String[] args) {
+        
+    }
+}
