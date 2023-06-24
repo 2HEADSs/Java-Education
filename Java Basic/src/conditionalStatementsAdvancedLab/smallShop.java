404995b77fa2d1fa1ee9fc46076c5e130a8fd07f
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-06-24 16:01:13 +0300

    Update smallShop.java

--- Java Basic/src/conditionalStatementsAdvancedLab/smallShop.java
+++ Java Basic/src/conditionalStatementsAdvancedLab/smallShop.java
@@ -9,33 +9,44 @@ public class smallShop {
 
         String product = scanner.nextLine();
         String town = scanner.nextLine();
-        double count = Double.parseDouble(scanner.nextLine());
+        double quantity = Double.parseDouble(scanner.nextLine());
+        double price = 0;
 
         if(town.equals("Sofia")){
             if(product.equals("coffee")){
-
+                 price = 0.50;
             } else  if(product.equals("water")){
-
+                price = 0.80;
             } else  if(product.equals("beer")){
-
+                price = 1.20;
             }else  if(product.equals("sweets")){
-
+                price = 1.45;
             }else  if(product.equals("peanuts")){
-
+                price = 1.60;
             }
         } else  if(town.equals("Plovdiv")){
-            if(age >=16){
-                System.out.println("Ms.");
-
-            } else  if(age < 16){
-                System.out.println("Miss");
+            if(product.equals("coffee")){
+                price = 0.40;
+            } else  if(product.equals("water")){
+                price = 0.70;
+            } else  if(product.equals("beer")){
+                price = 1.15;
+            }else  if(product.equals("sweets")){
+                price = 1.30;
+            }else  if(product.equals("peanuts")){
+                price = 1.50;
             }
         } else  if(town.equals("Varna")){
-            if(age >=16){
-                System.out.println("Ms.");
-
-            } else  if(age < 16){
-                System.out.println("Miss");
+            if(product.equals("coffee")){
+                price = 0.45;
+            } else  if(product.equals("water")){
+                price = 0.70;
+            } else  if(product.equals("beer")){
+                price = 1.10;
+            }else  if(product.equals("sweets")){
+                price = 1.35;
+            }else  if(product.equals("peanuts")){
+                price = 1.55;
             }
         }
 
