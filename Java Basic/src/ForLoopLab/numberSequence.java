4ae2aa790655e5f60c7715da2876c70a609ed339
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-06-25 17:17:29 +0300

    Create numberSequence.java

---
+++ Java Basic/src/ForLoopLab/numberSequence.java
@@ -0,0 +1,15 @@
+package ForLoopLab;
+
+import java.util.Scanner;
+
+public class numberSequence {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            
+        }
+    }
+}
