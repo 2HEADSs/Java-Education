5d2328ec326f6be5e29c2f994802d77eb09eedd8
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-06-25 16:11:24 +0300

    Update numbersfrom1to100.java

--- Java Basic/src/ForLoopLab/numbersfrom1to100.java
+++ Java Basic/src/ForLoopLab/numbersfrom1to100.java
@@ -6,7 +6,7 @@ public class numbersfrom1to100 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        for(int i= 1; i<=100; i ++){
+        for(int i = 1; i <= 100; i ++){
             System.out.println(i);
         }
     }
