444af63b923eab5d50cbabca9815ad7860226bfd
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-06-25 16:58:11 +0300

    Update characterSequence.java

--- Java Basic/src/ForLoopLab/characterSequence.java
+++ Java Basic/src/ForLoopLab/characterSequence.java
@@ -1,4 +1,15 @@
 package ForLoopLab;
 
+import java.util.Scanner;
+
 public class characterSequence {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        int textLength = text.length();
+
+        for (int i = 0; i < textLength; i++) {
+            System.out.println(text.charAt(i));
+        }
+    }
 }
