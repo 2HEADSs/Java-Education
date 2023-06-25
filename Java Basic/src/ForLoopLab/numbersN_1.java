26aea17cb1c48d9957ef50d2834cfd8599b82082
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-06-25 16:21:17 +0300

    Update numbersN_1.java

--- Java Basic/src/ForLoopLab/numbersN_1.java
+++ Java Basic/src/ForLoopLab/numbersN_1.java
@@ -1,4 +1,14 @@
 package ForLoopLab;
 
+import java.util.Scanner;
+
 public class numbersN_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = Integer.parseInt(scanner.nextLine());
+
+        for(int i = num; i >= 1; i --){
+            System.out.println(i);
+        }
+    }
 }
