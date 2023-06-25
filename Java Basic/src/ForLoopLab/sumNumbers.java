5ae853d242e406a09d0e126adfc798a753fca599
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-06-25 17:15:36 +0300

    Update sumNumbers.java

--- Java Basic/src/ForLoopLab/sumNumbers.java
+++ Java Basic/src/ForLoopLab/sumNumbers.java
@@ -1,4 +1,16 @@
 package ForLoopLab;
 
+import java.util.Scanner;
+
 public class sumNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += Integer.parseInt(scanner.nextLine());
+        }
+        System.out.println(sum);
+    }
 }
