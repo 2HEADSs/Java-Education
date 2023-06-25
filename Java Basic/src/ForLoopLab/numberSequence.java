7c5ea4bc058979652e26ca2118f740abeab0d0f4
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-06-25 17:22:48 +0300

    Update numberSequence.java

--- Java Basic/src/ForLoopLab/numberSequence.java
+++ Java Basic/src/ForLoopLab/numberSequence.java
@@ -7,9 +7,21 @@ public class numberSequence {
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
 
         for (int i = 0; i < n; i++) {
-            
+            int currentNum = Integer.parseInt(scanner.nextLine());
+            if(currentNum< min){
+                min = currentNum;
+            }
+
+            if(currentNum>max){
+                max = currentNum;
+            }
         }
+
+        System.out.printf("Max number: %d%n", max);
+        System.out.printf("Min number: %d%n", min);
     }
 }
