0d637a99b8dba75b8406b1ea14a6f5decc5dfc15
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-06-29 16:31:40 +0300

    Update sequence2_1.java

--- Java Basic/src/WhileLoop/sequence2_1.java
+++ Java Basic/src/WhileLoop/sequence2_1.java
@@ -1,4 +1,16 @@
 package WhileLoop;
 
+import java.util.Scanner;
+
 public class sequence2_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = Integer.parseInt(scanner.nextLine());
+        int initialNumber = 1;
+        while (num >= initialNumber){
+        System.out.println(initialNumber);
+        initialNumber = initialNumber * 2 + 1;
+        }
+    }
 }
