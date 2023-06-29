152e6a468eb206e21fe8448dace3be029b942531
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-06-29 16:09:15 +0300

    Update password.java

--- Java Basic/src/WhileLoop/password.java
+++ Java Basic/src/WhileLoop/password.java
@@ -1,4 +1,18 @@
 package WhileLoop;
 
+import java.util.Scanner;
+
 public class password {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        String pass = scanner.nextLine();
+        String input = scanner.nextLine();
+
+        while (!input.equals(pass)){
+            input = scanner.nextLine();
+        }
+            System.out.printf("Welcome %s!", name);
+    }
 }
