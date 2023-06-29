500c8e6ac6dc42dfe2d13cbf2284f43d783ec6eb
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-06-29 16:05:11 +0300

    Update readText.java

--- Java Basic/src/WhileLoop/readText.java
+++ Java Basic/src/WhileLoop/readText.java
@@ -1,7 +1,17 @@
 package WhileLoop;
 
+import java.util.Scanner;
+
 public class readText {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        while (true){
+            String input = scanner.nextLine();
+            if(input.equals("Stop")){
+                break;
+            }
+            System.out.println(input);
+        }
     }
 }
