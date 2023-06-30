791af8adc7ed3bd97bc9284c8fa5f81556427d8c
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-06-30 16:25:54 +0300

    Update maxNumber.java

--- Java Basic/src/WhileLoop/maxNumber.java
+++ Java Basic/src/WhileLoop/maxNumber.java
@@ -1,4 +1,22 @@
 package WhileLoop;
 
+import java.util.Scanner;
+
 public class maxNumber {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        int maxNum = Integer.MIN_VALUE;
+        while(!input.equals("Stop")){
+            int currentNum = Integer.parseInt(input);
+            if(currentNum>maxNum){
+                maxNum = currentNum;
+            }
+            input = scanner.nextLine();
+        }
+        System.out.println(maxNum);
+    }
 }
