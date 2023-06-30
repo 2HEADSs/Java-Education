b8dfc764c1721415555ae8cfd54350dee338f514
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-06-30 16:53:08 +0300

    Update graduation.java

--- Java Basic/src/WhileLoop/graduation.java
+++ Java Basic/src/WhileLoop/graduation.java
@@ -8,5 +8,28 @@ public class graduation {
         Scanner scanner = new Scanner(System.in);
 
         String name = scanner.nextLine();
+        double sum = 0;
+        int year = 1;
+        int excluded = 0;
+
+        while(year<=12){
+        if(excluded >1){
+            break;
+        }
+        double currentGrade = Double.parseDouble(scanner.nextLine());
+        if(currentGrade<=3){
+            excluded++;
+            continue;
+        }
+        sum+= currentGrade;
+        year++;
+        }
+
+        if(excluded<1){
+        double average = sum/12;
+        System.out.printf("%s graduated. Average grade: %.2f", name, average);
+        }else{
+            System.out.printf("%s has been excluded at %d grade%n", name, year);
+        }
     }
 }
