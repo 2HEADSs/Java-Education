52fbefab5712caf9c6579a571d174b465648c243
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-06-30 16:33:42 +0300

    Update graduation.java

--- Java Basic/src/WhileLoop/graduation.java
+++ Java Basic/src/WhileLoop/graduation.java
@@ -1,4 +1,12 @@
 package WhileLoop;
 
+import java.util.Scanner;
+
 public class graduation {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+    }
 }
