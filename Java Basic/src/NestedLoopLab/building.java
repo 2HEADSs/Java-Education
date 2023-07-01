5bad4a72b3aea6e9cfc69b63ad5764c0492d8c96
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-07-01 15:03:27 +0300

    Update building.java

--- Java Basic/src/NestedLoopLab/building.java
+++ Java Basic/src/NestedLoopLab/building.java
@@ -1,4 +1,19 @@
 package NestedLoopLab;
 
+import java.util.Scanner;
+
 public class building {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int floors = Integer.parseInt((scanner.nextLine()));
+        int rooms = Integer.parseInt((scanner.nextLine()));
+
+        for(int f = floors; f >= 1; f--){
+            for(int r = 0; r<rooms; r++){
+                System.out.printf("%d%d", f, r);
+            }
+        }
+    }
+
+
 }
