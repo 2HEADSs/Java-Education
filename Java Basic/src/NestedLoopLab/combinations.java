0e1826e4ffc533605af557bec42836bfeae870ef
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-07-01 14:31:56 +0300

    Update combinations.java

--- Java Basic/src/NestedLoopLab/combinations.java
+++ Java Basic/src/NestedLoopLab/combinations.java
@@ -1,4 +1,19 @@
 package NestedLoopLab;
 
+import java.util.Scanner;
+
 public class combinations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j <=n; j++){
+                for (int k = 0; k <=n; k++){
+                System.out.printf("%d-%d-%d%n",i,j,k);
+                }
+
+            }
+        }
+    }
 }
