89f711dc64fccdb47fd51284ab474ad082f9c8cb
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-07-01 14:38:24 +0300

    Create sumOfTwoNumbers.java

---
+++ Java Basic/src/NestedLoopLab/sumOfTwoNumbers.java
@@ -0,0 +1,14 @@
+package NestedLoopLab;
+
+import java.util.Scanner;
+
+public class sumOfTwoNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int firstNum = Integer.parseInt(scanner.nextLine());
+        int secondNum = Integer.parseInt(scanner.nextLine());
+        int thirdNum = Integer.parseInt(scanner.nextLine());
+
+
+    }
+}
