d62468234a883d611ab17a990b0d6a10cc916f0e
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-07-01 14:46:17 +0300

    Update sumOfTwoNumbers.java

--- Java Basic/src/NestedLoopLab/sumOfTwoNumbers.java
+++ Java Basic/src/NestedLoopLab/sumOfTwoNumbers.java
@@ -5,10 +5,28 @@ import java.util.Scanner;
 public class sumOfTwoNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int firstNum = Integer.parseInt(scanner.nextLine());
-        int secondNum = Integer.parseInt(scanner.nextLine());
-        int thirdNum = Integer.parseInt(scanner.nextLine());
+        int start = Integer.parseInt(scanner.nextLine());
+        int end = Integer.parseInt(scanner.nextLine());
+        int magicNum = Integer.parseInt(scanner.nextLine());
 
-
+        int count = 0;
+        boolean flag = false;
+        for(int i = start; i <=end; i++){
+            for(int j = start; j <=end; j++){
+                count++;
+                int sum = i +j;
+                if(sum == magicNum){
+                flag = true;
+                System.out.printf("Combination N:%d (%d + %d = %d)%n", count, i, j, sum);
+                break;
+                }
+            }
+            if(flag){
+                    break;
+            }
+        }
+        if(!flag){
+            System.out.printf("%d combinations - neither equals %d", count, magicNum);
+        }
     }
 }
