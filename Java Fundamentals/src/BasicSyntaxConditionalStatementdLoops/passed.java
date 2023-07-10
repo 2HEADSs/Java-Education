c577234b51d8c8973903609201dcdbf6c6d4e4fa
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-07-10 18:07:36 +0300

    Update passed.java

--- Java Fundamentals/src/BasicSyntaxConditionalStatementdLoops/passed.java
+++ Java Fundamentals/src/BasicSyntaxConditionalStatementdLoops/passed.java
@@ -5,5 +5,10 @@ import java.util.Scanner;
 public class passed {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        double grade = scanner.nextDouble();
+
+        if (grade >=3.00){
+            System.out.println("Passed!");
+        }
     }
 }
