d8cfe4f28bf1e89d3253985d70ead06b7781d14c
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-07-10 18:31:32 +0300

    Update passedOrFailed.java

--- Java Fundamentals/src/BasicSyntaxConditionalStatementdLoops/passedOrFailed.java
+++ Java Fundamentals/src/BasicSyntaxConditionalStatementdLoops/passedOrFailed.java
@@ -3,5 +3,14 @@ package BasicSyntaxConditionalStatementdLoops;
 import java.util.Scanner;
 
 public class passedOrFailed {
-    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double grade = scanner.nextDouble();
+
+        if (grade >=3.00){
+            System.out.println("Passed!");
+        } else  {
+            System.out.println("Failed!");
+        }
+    }
 }
