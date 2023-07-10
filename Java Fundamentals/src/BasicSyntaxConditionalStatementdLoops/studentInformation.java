0a7a158a9804f1ab9f4033ada616888a1d04a6b8
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-07-10 17:42:13 +0300

    Update studentInformation.java

--- Java Fundamentals/src/BasicSyntaxConditionalStatementdLoops/studentInformation.java
+++ Java Fundamentals/src/BasicSyntaxConditionalStatementdLoops/studentInformation.java
@@ -3,5 +3,7 @@ package BasicSyntaxConditionalStatementdLoops;
 public class studentInformation {
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+
+    String name = scanner.nextLine();
     }
 }
