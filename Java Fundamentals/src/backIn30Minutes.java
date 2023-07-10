37e2e454c52a0812bf81ffdd31b27d63df9899e0
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-07-10 18:12:51 +0300

    Update backIn30Minutes.java

--- Java Fundamentals/src/backIn30Minutes.java
+++ Java Fundamentals/src/backIn30Minutes.java
@@ -1,5 +1,16 @@
 import java.util.Scanner;
 
 public class backIn30Minutes {
+    public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+
+    int hours = scanner.nextInt();
+    int minutes = scanner.nextInt();
+
+
+
+        System.out.printf("%d:%d", hours, minutes);
+    }
+
+
 }
