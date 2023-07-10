352386b961d2d45e34da9f8dd94d24a085f97c78
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-07-10 17:51:13 +0300

    Create passed.java

---
+++ Java Fundamentals/src/BasicSyntaxConditionalStatementdLoops/passed.java
@@ -0,0 +1,9 @@
+package BasicSyntaxConditionalStatementdLoops;
+
+import java.util.Scanner;
+
+public class passed {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
