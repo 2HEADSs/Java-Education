6496b4649a9dcc8add70430a1b516f7fa7a9955a
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-07-12 10:02:14 +0300

    Update theatrePromotion.java

--- Java Fundamentals/src/BasicSyntaxConditionalStatementdLoops/theatrePromotion.java
+++ Java Fundamentals/src/BasicSyntaxConditionalStatementdLoops/theatrePromotion.java
@@ -12,37 +12,50 @@ public class theatrePromotion {
         boolean error = false;
 
 
-        switch (day){
+        switch (day) {
 
             case "Weekday":
-                if(age<=0){
+                if (age <= 0) {
                     error = true;
                     break;
                 }
-                if(age <=18){
+                if (age <= 18) {
                     price = 12;
-                } else if (age <=64){
+                } else if (age <= 64) {
                     price = 18;
-                }
-                else if (age <=122){
+                } else if (age <= 122) {
                     price = 12;
                 }
-            case "Holiday":
-                if(age<=0){
+                break;
+            case "Weekend":
+                if (age <= 0) {
                     error = true;
                     break;
                 }
-                if(age <=18){
+                if (age <= 18) {
                     price = 15;
-                } else if (age <=64){
+                } else if (age <= 64) {
                     price = 20;
-                }
-                else if (age <=122){
+                } else if (age <= 122) {
                     price = 15;
                 }
+                break;
+            case "Holiday":
+                if (age <= 0) {
+                    error = true;
+                    break;
+                }
+                if (age <= 18) {
+                    price = 5;
+                } else if (age <= 64) {
+                    price = 12;
+                } else if (age <= 122) {
+                    price = 10;
+                }
+                break;
         }
 
-        if(!error){
+        if (!error) {
             System.out.printf("%d$", price);
         } else {
             System.out.println("Error!");
