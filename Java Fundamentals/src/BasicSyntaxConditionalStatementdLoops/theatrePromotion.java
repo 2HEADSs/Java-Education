e181e97cdb5199667f097aa642db0215686342ae
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-07-12 09:54:19 +0300

    Update theatrePromotion.java

--- Java Fundamentals/src/BasicSyntaxConditionalStatementdLoops/theatrePromotion.java
+++ Java Fundamentals/src/BasicSyntaxConditionalStatementdLoops/theatrePromotion.java
@@ -1,4 +1,51 @@
 package BasicSyntaxConditionalStatementdLoops;
 
+import java.util.Scanner;
+
 public class theatrePromotion {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String day = scanner.nextLine();
+        int age = scanner.nextInt();
+        int price = 0;
+        boolean error = false;
+
+
+        switch (day){
+
+            case "Weekday":
+                if(age<=0){
+                    error = true;
+                    break;
+                }
+                if(age <=18){
+                    price = 12;
+                } else if (age <=64){
+                    price = 18;
+                }
+                else if (age <=122){
+                    price = 12;
+                }
+            case "Holiday":
+                if(age<=0){
+                    error = true;
+                    break;
+                }
+                if(age <=18){
+                    price = 15;
+                } else if (age <=64){
+                    price = 20;
+                }
+                else if (age <=122){
+                    price = 15;
+                }
+        }
+
+        if(!error){
+            System.out.printf("%d$", price);
+        } else {
+            System.out.println("Error!");
+        }
+    }
 }
