9c2287e7f5da43a8a40c819e0b7eb89e920b8e0c
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-07-12 10:33:15 +0300

    Update sumOfOddNumbers.java

--- Java Fundamentals/src/BasicSyntaxConditionalStatementdLoops/sumOfOddNumbers.java
+++ Java Fundamentals/src/BasicSyntaxConditionalStatementdLoops/sumOfOddNumbers.java
@@ -1,4 +1,19 @@
 package BasicSyntaxConditionalStatementdLoops;
 
+import java.util.Scanner;
+
 public class sumOfOddNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int countNum = scanner.nextInt();
+        int num = 1;
+        int sum = 0;
+
+        for (int i = 1; i <= countNum; i++) {
+            System.out.println(num);
+            sum += num;
+            num += 2;
+        }
+        System.out.printf("Sum: %d", sum);
+    }
 }
