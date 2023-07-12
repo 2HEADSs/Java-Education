537b02110eef864307a88a9d90558002319c2d56
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-07-12 10:27:28 +0300

    Update divisibleby3.java

--- Java Fundamentals/src/BasicSyntaxConditionalStatementdLoops/divisibleby3.java
+++ Java Fundamentals/src/BasicSyntaxConditionalStatementdLoops/divisibleby3.java
@@ -1,4 +1,9 @@
 package BasicSyntaxConditionalStatementdLoops;
 
 public class divisibleby3 {
+    public static void main(String[] args) {
+        for(int i = 3; i <=100; i +=3){
+            System.out.println(i);
+        }
+    }
 }
