ba6508a75a4af4325ee184a5e98db6b5675051c0
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-09-17 23:19:13 +0300

    Create test.java

---
+++ Java fundamenatls/src/DataTypesAndVariablesLab/test.java
@@ -0,0 +1,13 @@
+package DataTypesAndVariablesLab;
+
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+            byte b = 127;
+            b += 130;
+
+        System.out.println(b);
+    }
+}
