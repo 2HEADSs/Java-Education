1cd6d5ef6c252c1293f366b2f9b6e48ac07c4ee2
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-09-21 16:56:28 +0300

    Update ExactSumOfRealNums.java

--- Java fundamenatls/src/DataTypesAndVariablesLab/ExactSumOfRealNums.java
+++ Java fundamenatls/src/DataTypesAndVariablesLab/ExactSumOfRealNums.java
@@ -1,6 +1,7 @@
 package DataTypesAndVariablesLab;
 
 
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class ExactSumOfRealNums {
@@ -8,6 +9,13 @@ public class ExactSumOfRealNums {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int n = Integer.parseInt(scanner.nextLine());
+        BigDecimal sum = new BigDecimal(0);
 
+        for (int i = 0; i < n; i++) {
+            BigDecimal currentNum = new BigDecimal(scanner.nextLine());
+            sum = sum.add(currentNum);
+        }
+        System.out.println(sum);
     }
 }
