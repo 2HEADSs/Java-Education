9ae1c70851ca8cf5d18f04f11ed22a7233e0fc3c
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-09-21 18:06:52 +0300

    Update SpecialNums.java

--- Java fundamenatls/src/DataTypesAndVariablesLab/SpecialNums.java
+++ Java fundamenatls/src/DataTypesAndVariablesLab/SpecialNums.java
@@ -3,8 +3,31 @@ package DataTypesAndVariablesLab;
 import java.util.Scanner;
 
 public class SpecialNums {
-    Scanner scanner = new Scanner(System.in);
-    String input = scanner.nextLine();
-    int number = Integer.parseInt(input);
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        int n = Integer.parseInt(input);
+
+
+        for (int i = 0; i < n; i++) {
+            int number = i;
+            int sum = 0;
+            while (number > 0) {
+                //take the last symbol 123 % 10 = 3 (INTEGER)
+                int digit = number % 10;
+
+                sum += digit;
+                //remove last symbol 123/10 = 12 (INTEGER)
+                number /= 10;
+            }
+            if(sum == 5 || sum == 11 || sum == 7 ){
+                System.out.println(i + " -> True" );
+            } else {
+                System.out.printf("%d -> False" , i);
+            }
+
+        }
+
+    }
+
 }
