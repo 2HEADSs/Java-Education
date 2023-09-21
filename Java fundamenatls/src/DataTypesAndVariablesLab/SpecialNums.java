538a0253c98f13082c868a7da54bde087698130b
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-09-21 17:47:33 +0300

    Create SpecialNums.java

---
+++ Java fundamenatls/src/DataTypesAndVariablesLab/SpecialNums.java
@@ -0,0 +1,10 @@
+package DataTypesAndVariablesLab;
+
+import java.util.Scanner;
+
+public class SpecialNums {
+    Scanner scanner = new Scanner(System.in);
+    String input = scanner.nextLine();
+    int number = Integer.parseInt(input);
+    
+}
