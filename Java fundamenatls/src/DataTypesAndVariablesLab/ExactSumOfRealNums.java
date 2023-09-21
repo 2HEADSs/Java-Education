de96e748058a134c3fc574fb1d2a0626fd813558
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-09-21 16:50:05 +0300

    Create ExactSumOfRealNums.java

---
+++ Java fundamenatls/src/DataTypesAndVariablesLab/ExactSumOfRealNums.java
@@ -0,0 +1,13 @@
+package DataTypesAndVariablesLab;
+
+
+import java.util.Scanner;
+
+public class ExactSumOfRealNums {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
