0cc83b9fab0721e2f80418574f261fc6f506b950
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-09-21 16:23:20 +0300

    Update ConvertMetersToKilometers.java

--- Java fundamenatls/src/DataTypesAndVariablesLab/ConvertMetersToKilometers.java
+++ Java fundamenatls/src/DataTypesAndVariablesLab/ConvertMetersToKilometers.java
@@ -10,7 +10,7 @@ public class ConvertMetersToKilometers {
 
         int meters = scanner.nextInt();
 
-        double km = meters / 1000D;
+        double km = meters / 1000.0;
         System.out.println(km);
     }
 }
