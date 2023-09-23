e081d4f7337d9ff12d12e54d78be17214477b29f
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-09-23 14:32:37 +0300

    Update dataTypeFinder.java

--- Java fundamenatls/src/DataTypesAndVariablesMoreExersice/dataTypeFinder.java
+++ Java fundamenatls/src/DataTypesAndVariablesMoreExersice/dataTypeFinder.java
@@ -10,7 +10,30 @@ public class dataTypeFinder {
 
         while (!input.equals("END")) {
             String type = "";
-            if(input.equals("true") || input.equals("false")){}
+            if (input.equalsIgnoreCase("true") || input.equalsIgnoreCase("false")) {
+                type = "boolean";
+            } else if (input.length() == 1) {
+                char symbol = input.charAt(0);
+                if(symbol < 47 || symbol >58){
+                    type = "character";
+                }
+                else {
+                    type = "integer";
+                }
+            } else {
+                boolean isString = false;
+                boolean isFloat = false;
+                for (int i = 0; i < input.length(); i++) {
+                    char currentSymbol = input.charAt(i);
+
+                    if(currentSymbol < 47 || currentSymbol >58) {
+                        isString = true;
+                    }
+                    if(currentSymbol == 46){
+                        isFloat = true;
+                    }
+                }
+            }
             input = scanner.nextLine();
 
         }
