6b37d2f56c944709c8b5f605ae92954a8a769c06
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-09-23 14:06:08 +0300

    Update DataFinder

--- Java fundamenatls/src/DataTypesAndVariablesMoreExersice/dataTypeFinder.java
+++ Java fundamenatls/src/DataTypesAndVariablesMoreExersice/dataTypeFinder.java
@@ -1,4 +1,18 @@
 package DataTypesAndVariablesMoreExersice;
 
+import java.util.Scanner;
+
 public class dataTypeFinder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        while (!input.equals("END") ){
+            String type = "";
+
+            input = scanner.nextLine();
+        }
+
+    }
 }
