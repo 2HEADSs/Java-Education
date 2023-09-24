94864a6a6d141e1dcb28ebb9bdaf95bb27db46f4
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-09-24 13:44:36 +0300

    Update equalArrays.java

--- Java fundamenatls/src/Arrays/equalArrays.java
+++ Java fundamenatls/src/Arrays/equalArrays.java
@@ -5,5 +5,28 @@ import java.util.Scanner;
 public class equalArrays {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String firstNums = scanner.nextLine();
+        String secondNums = scanner.nextLine();
+        String[] firstArray = firstNums.split(" ");
+        String[] secondArray = secondNums.split(" ");
+
+        boolean areIdentical = true;
+        int sum = 0;
+
+        for (int i = 0; i < firstArray.length - 1; i++) {
+
+            if(Integer.parseInt(firstArray[i]) == Integer.parseInt(secondArray[i])){
+                sum += Integer.parseInt(firstArray[i]) + Integer.parseInt(secondArray[i]);
+            } else {
+                areIdentical = false;
+                System.out.printf("Arrays are not identical. Found difference at %d index.", i);
+                break;
+            }
+        }
+        if(areIdentical){
+            System.out.printf("Arrays are identical. Sum: %d", sum);
+        }
+
     }
 }
+
