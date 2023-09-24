f1080fcd30513c0b28da84b05922521f28140245
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-09-24 13:46:06 +0300

    Update equalsArays2.java

--- Java fundamenatls/src/Arrays/equalsArays2.java
+++ Java fundamenatls/src/Arrays/equalsArays2.java
@@ -1,4 +1,29 @@
 package Arrays;
 
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class equalsArays2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] firstArr = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] secondArr = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int sum = 0;
+        boolean areIdentical = true;
+
+        for (int i = 0; i < firstArr.length; i++) {
+            sum += firstArr[i];
+
+            if (firstArr[i] != secondArr[i]) {
+                areIdentical = false;
+                System.out.printf("Arrays are not identical. Found difference at %d index.", i);
+                break;
+            }
+        }
+        if (areIdentical) {
+            System.out.printf("Arrays are identical. Sum: %d", sum);
+        }
+    }
 }
