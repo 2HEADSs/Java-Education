13cbc46b8aea3c2f001ae5db196bfa3cb4874963
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-09-24 08:37:46 +0300

    Create test2.java

---
+++ Java fundamenatls/src/Arrays/test2.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class test2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String nums = scanner.nextLine();
+        int num2 = scanner.nextInt();
+        System.out.println(nums);
+        System.out.println(num2);
+    }
+}
