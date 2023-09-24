36513e8bed6454e68d1485f6110ca9f876eb3454
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-09-24 09:28:31 +0300

    Update test3.java

--- Java fundamenatls/src/Arrays/test3.java
+++ Java fundamenatls/src/Arrays/test3.java
@@ -27,7 +27,11 @@ public class test3 {
 
         }
         for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i] + " ");
+            System.out.println(numbers[i] + "for ");
         }
+        for(int number : numbers){
+            System.out.println(number + "forEach ");
+        }
+
     }
 }
