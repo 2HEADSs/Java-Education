48e4fd98b07c463aa697d8d915150687d8e3b47d
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-09-24 09:01:26 +0300

    Update test3.java

--- Java fundamenatls/src/Arrays/test3.java
+++ Java fundamenatls/src/Arrays/test3.java
@@ -7,13 +7,16 @@ public class test3 {
         Scanner scanner = new Scanner(System.in);
         String lineOfNumber = scanner.nextLine();
 
-        String[] numberAsString = lineOfNumber.split(" ");
+        String[] numberAsString = lineOfNumber.split("\\s+");
 
         int [] numbers = new int[numberAsString.length];
 
         for (int i = 0; i < numbers.length; i++) {
                 numbers[i]= Integer.parseInt(numberAsString[i]);
         }
-        
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+
+        }
     }
 }
