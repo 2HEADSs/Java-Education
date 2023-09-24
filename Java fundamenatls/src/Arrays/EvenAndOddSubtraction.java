21c6cab772b6993782a323abb32bfa1d50583d36
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-09-24 12:48:17 +0300

    Create EvenAndOddSubtraction.java

---
+++ Java fundamenatls/src/Arrays/EvenAndOddSubtraction.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class EvenAndOddSubtraction {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        String[] numbersAsString = line.split("\\s+");
+
+
+        int evenSum = 0;
+        int oddSum = 0;
+        for (String s : numbersAsString) {
+            int number = Integer.parseInt(s);
+            if (number % 2 == 0) {
+                evenSum += number;
+            } else {
+                oddSum += number;
+            }
+        }
+        int resul = evenSum-oddSum;
+
+
+    }
+}
