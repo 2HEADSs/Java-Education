f8c52c02ab714dd0fd2ede43842ab217e8a18c4e
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-09-24 14:21:44 +0300

    Create condenseArraTNumber.java

---
+++ Java fundamenatls/src/Arrays/condenseArraTNumber.java
@@ -0,0 +1,9 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class condenseArraTNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
