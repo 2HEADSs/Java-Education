a42bb6ca48d18da97f9e149824528902f28db7cb
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-09-25 17:45:29 +0300

    Update zigZagArrays.java

--- Java fundamenatls/src/ArraysExercise/zigZagArrays.java
+++ Java fundamenatls/src/ArraysExercise/zigZagArrays.java
@@ -7,9 +7,25 @@ public class zigZagArrays {
         Scanner scanner = new Scanner(System.in);
         int countArrays = Integer.parseInt(scanner.nextLine());
 
+        String[] firstArray = new String[countArrays];
+        String[] secondArray = new String[countArrays];
 
         for (int i = 0; i < countArrays; i++) {
-            String [] line= scanner.nextLine().split(" ");
+            String[] line = scanner.nextLine().split(" ");
+            if (i % 2 == 0) {
+                firstArray[i] = line[0];
+                secondArray[i] = line[1];
+            } else  {
+                firstArray[i] = line[1];
+                secondArray[i] = line[0];
+            }
+        }
+        for (String elFirstArr: firstArray){
+            System.out.printf(elFirstArr +" ");
+        }
+        System.out.println();
+        for (String elSecondArr: secondArray){
+            System.out.printf(elSecondArr+" ");
         }
     }
 }
