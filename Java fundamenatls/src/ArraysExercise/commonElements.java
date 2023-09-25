0bfd494f1806ebb8f197e9b36e53f7c6cf5d680f
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-09-25 17:04:16 +0300

    Update commonElements.java

--- Java fundamenatls/src/ArraysExercise/commonElements.java
+++ Java fundamenatls/src/ArraysExercise/commonElements.java
@@ -5,5 +5,19 @@ import java.util.Scanner;
 public class commonElements {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String firstLine = scanner.nextLine();
+        String secondLine = scanner.nextLine();
+
+        String[] firstArray = firstLine.split(" ");
+        String[] secondArray = secondLine.split(" ");
+
+
+        for (String firstEl : firstArray) {
+            for (String secondEl : secondArray) {
+                if (firstEl.equals(secondEl)) {
+                    System.out.print(firstEl + " ");
+                }
+            }
+        }
     }
 }
