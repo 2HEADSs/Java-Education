7e6540f1f4b204396f496ff7125b9db30b2df86a
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-09-25 17:50:49 +0300

    Create arrayRotation.java

---
+++ Java fundamenatls/src/ArraysExercise/arrayRotation.java
@@ -0,0 +1,12 @@
+package ArraysExercise;
+
+import java.util.Scanner;
+
+public class arrayRotation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String [] input = scanner.nextLine().split(" ");
+        int cycles = scanner.nextInt();
+        System.out.println(cycles);
+    }
+}
