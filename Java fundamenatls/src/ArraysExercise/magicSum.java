2d6ddc6e8a43e074d9d00d98ba8267fcd899de0c
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-09-25 18:47:55 +0300

    Create magicSum.java

---
+++ Java fundamenatls/src/ArraysExercise/magicSum.java
@@ -0,0 +1,10 @@
+package ArraysExercise;
+
+import java.util.Scanner;
+
+public class magicSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
