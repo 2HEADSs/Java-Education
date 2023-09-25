fae15fec2beedc22d287d8967f43a216e2b93598
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-09-25 16:49:46 +0300

    Update train.java

--- Java fundamenatls/src/ArraysExercise/train.java
+++ Java fundamenatls/src/ArraysExercise/train.java
@@ -9,7 +9,17 @@ public class train {
 
         int[] train = new int[countWagons];
 
-        
-
+        int countPassenger = 0;
+        for (int i = 0; i < train.length; i++) {
+            train[i] = Integer.parseInt(scanner.nextLine());
+            countPassenger += train[i];
+        }
+        for (int i = 0; i < train.length; i++) {
+            System.out.print(train[i] +" ");
+        }
+        System.out.println();
+        System.out.println(countPassenger);
+//        add empty line before "countPassenger"
+//        System.out.printf("\n%s", countPassenger);
     }
 }
