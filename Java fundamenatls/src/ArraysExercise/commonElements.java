1b83758f05f99839b69f7bbf1332eadbd3a6cfe3
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-09-25 16:58:57 +0300

    Create commonElements.java

---
+++ Java fundamenatls/src/ArraysExercise/commonElements.java
@@ -0,0 +1,9 @@
+package ArraysExercise;
+
+import java.util.Scanner;
+
+public class commonElements {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
