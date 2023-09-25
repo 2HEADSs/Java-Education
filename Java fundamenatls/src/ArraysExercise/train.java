ee0a3b63c2a3d760256a9695fed1a41efaa47c67
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-09-25 16:43:32 +0300

    Update train.java

--- Java fundamenatls/src/ArraysExercise/train.java
+++ Java fundamenatls/src/ArraysExercise/train.java
@@ -5,7 +5,11 @@ import java.util.Scanner;
 public class train {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int countWagons = Integer.parseInt(scanner.nextLine());
 
+        int[] train = new int[countWagons];
+
+        
 
     }
 }
