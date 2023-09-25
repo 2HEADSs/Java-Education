001c2b1b175b25ae015c60e2df53a487ab18d738
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-09-25 18:58:24 +0300

    Update magicSum.java

--- Java fundamenatls/src/ArraysExercise/magicSum.java
+++ Java fundamenatls/src/ArraysExercise/magicSum.java
@@ -1,10 +1,27 @@
 package ArraysExercise;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class magicSum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        int[] numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(e -> Integer.parseInt(e)).toArray();
+        int magicSum = Integer.parseInt(scanner.nextLine());
+
+        for(int i = 0; i<numbers.length; i ++){;
+
+            int firstNum = numbers[i];
+
+            for(int j = i+1; j<numbers.length; j++){
+                int secondNum = numbers[j];
+                if(firstNum + secondNum == magicSum){
+                    System.out.printf(firstNum + " " + secondNum+ "\n");
+                }
+            }
+        };
+
+
     }
 }
