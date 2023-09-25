b6190a72adee232f10292f8a94cc6f56134f2b06
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-09-25 17:29:12 +0300

    Create zigZagArrays.java

---
+++ Java fundamenatls/src/ArraysExercise/zigZagArrays.java
@@ -0,0 +1,15 @@
+package ArraysExercise;
+
+import java.util.Scanner;
+
+public class zigZagArrays {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int countArrays = Integer.parseInt(scanner.nextLine());
+
+
+        for (int i = 0; i < countArrays; i++) {
+            String [] line= scanner.nextLine().split(" ");
+        }
+    }
+}
