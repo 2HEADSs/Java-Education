01b632b393305e8d7b1d279e5162a121055f95e6
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-10-03 22:34:27 +0300

    Update repeatString.java

--- Java fundamenatls/src/Methods/repeatString.java
+++ Java fundamenatls/src/Methods/repeatString.java
@@ -1,7 +1,27 @@
 package Methods;
 
+import java.util.Scanner;
+
 public class repeatString {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        int repetitions = Integer.parseInt(scanner.nextLine());
+
+        String s = scanner.nextLine();
+
+        String repeated = repeatedString(s, repetitions);
+
+        System.out.println(repeated);
     }
+
+    private static String repeatedString(String s, int repetitions) {
+        String[] strings = new String[repetitions];
+        for (int i = 0; i < strings.length; i++) {
+            strings[i] = s;
+        }
+        String repeated = String.join("", strings);
+        return repeated;
+    }
+
+    ;
 }
