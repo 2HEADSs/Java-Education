faf09a658aa69d499aa4b748545dc472b1275610
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-10-03 16:28:19 +0300

    Create signOfIntegerNumbers.java

---
+++ Java fundamenatls/src/Methods/signOfIntegerNumbers.java
@@ -0,0 +1,13 @@
+package Methods;
+
+public class signOfIntegerNumbers {
+    public static void main(String[] args) {
+
+    }
+
+    private static void  printArray(int [] numbers){
+        for(int number: numbers){
+            System.out.print(number + " ");
+        }
+    }
+}
