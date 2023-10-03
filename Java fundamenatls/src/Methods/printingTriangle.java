f3ca93d15aa3f1c388a68c27e4a742d012474fa2
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-10-03 21:07:46 +0300

    Update printingTriangle.java

--- Java fundamenatls/src/Methods/printingTriangle.java
+++ Java fundamenatls/src/Methods/printingTriangle.java
@@ -1,7 +1,22 @@
 package Methods;
 
+import java.util.Scanner;
+
 public class printingTriangle {
     public static void main(String[] args) {
+        int number = new Scanner(System.in).nextInt();
 
+
+        getPrintIncreasingNumbers(number);
+    }
+
+    private static void getPrintIncreasingNumbers(int length) {
+        for (int i = 1; i <= length; i++) {
+            System.out.println(i);
+            if (i < length) {
+                System.out.println(" ");
+
+            }
+        }
     }
 }
