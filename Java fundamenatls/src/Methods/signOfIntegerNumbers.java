4d32a6a887f2b2703555335691bc79fea503b659
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-10-03 16:31:08 +0300

    Update signOfIntegerNumbers.java

--- Java fundamenatls/src/Methods/signOfIntegerNumbers.java
+++ Java fundamenatls/src/Methods/signOfIntegerNumbers.java
@@ -1,8 +1,20 @@
 package Methods;
 
+import java.util.Scanner;
+
 public class signOfIntegerNumbers {
     public static void main(String[] args) {
+        int number = new Scanner(System.in).nextInt();
+        printSign(number);
+    }
 
+    private static void printSign(int number) {
+        if(number >= 0){
+            System.out.println("The number " + number + " is positive");
+        } else  {
+            System.out.println("The number " + number + " is negative");
+
+        }
     }
 
     private static void  printArray(int [] numbers){
