ee5e3c48e59f789078a3e04787ba165b5f83a994
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-10-06 11:53:59 +0300

    Update calculations.java

--- Java fundamenatls/src/Methods/calculations.java
+++ Java fundamenatls/src/Methods/calculations.java
@@ -5,6 +5,21 @@ import java.util.Scanner;
 public class calculations {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        String command = scanner.nextLine();
+        int firstNum = Integer.parseInt(scanner.nextLine());
+        int secondNum = Integer.parseInt(scanner.nextLine());
+        int result = 0;
+
+
+        if (command.equals("add")) {
+            result = firstNum + secondNum;
+        } else if (command.equals("multiply")) {
+            result = firstNum * secondNum;
+        } else if (command.equals("subtract")) {
+            result = firstNum - secondNum;
+        } else if (command.equals("divide")) {
+            result = firstNum / secondNum;
+        }
+        System.out.println(result);
     }
 }
