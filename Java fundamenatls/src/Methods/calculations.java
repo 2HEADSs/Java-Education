54027244c9e2341022c0787651cf33944647fecd
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-10-06 11:39:38 +0300

    Update calculations.java

--- Java fundamenatls/src/Methods/calculations.java
+++ Java fundamenatls/src/Methods/calculations.java
@@ -1,4 +1,10 @@
 package Methods;
 
+import java.util.Scanner;
+
 public class calculations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
 }
