e432c7a290c8cc331c4c23132578d2884ac7c2bc
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-10-08 14:11:58 +0300

    Update mathPower.java

--- Java fundamenatls/src/Methods/mathPower.java
+++ Java fundamenatls/src/Methods/mathPower.java
@@ -1,5 +1,6 @@
 package Methods;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class mathPower {
@@ -7,7 +8,15 @@ public class mathPower {
         Scanner scanner = new Scanner(System.in);
         double firstNum = Double.parseDouble(scanner.nextLine());
         double secondNum = Double.parseDouble(scanner.nextLine());
+        System.out.println(new DecimalFormat("0.####").format(output(firstNum,secondNum)));
 
     }
 
+    public static double output(double a, double b) {
+        double result = a;
+        for (int i = 1; i < b; i++) {
+            result *= a;
+        }
+        return result;
+    };
 }
