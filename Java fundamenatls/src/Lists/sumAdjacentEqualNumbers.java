776522e356590d2900f8b3c91c55c4173f0c39ea
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-10-08 22:18:57 +0300

    Update sumAdjacentEqualNumbers.java

--- Java fundamenatls/src/Lists/sumAdjacentEqualNumbers.java
+++ Java fundamenatls/src/Lists/sumAdjacentEqualNumbers.java
@@ -1,5 +1,9 @@
 package Lists;
 
+import java.util.Scanner;
+
 public class sumAdjacentEqualNumbers {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
 }
