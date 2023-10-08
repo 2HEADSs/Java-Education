7f555f2fae1e8d8b1c6ff84135268adcdd9ac0ca
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-10-08 13:49:00 +0300

    Update orders.java

--- Java fundamenatls/src/Methods/orders.java
+++ Java fundamenatls/src/Methods/orders.java
@@ -5,7 +5,38 @@ import java.util.Scanner;
 public class orders {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String product = scanner.nextLine();
+        int count = Integer.parseInt(scanner.nextLine());
+        switch (product) {
+            case "cofee":
+                coffe(count);
+                break;
+            case "water":
+                water(count);
+                break;
+            case "coke":
+                coke(count);
+                break;
+            case "snack":
+                snack(count);
+                break;
+        }
 
+    }
 
+    public static void coffe(int count) {
+        System.out.println(count * 1.50);
+    }
+
+    public static void water(int count) {
+        System.out.println(count * 1.00);
+    }
+
+    public static void coke(int count) {
+        System.out.println(count * 1.40);
+    }
+
+    public static void snack(int count) {
+        System.out.println(count * 2.00);
     }
 }
