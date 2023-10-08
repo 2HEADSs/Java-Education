640f8b7ce6b7996138af0ebda66a3eb44b63d932
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-10-08 13:42:44 +0300

    Update calculations.java

--- Java fundamenatls/src/Methods/calculations.java
+++ Java fundamenatls/src/Methods/calculations.java
@@ -8,38 +8,29 @@ public class calculations {
         String command = scanner.nextLine();
         int firstNum = Integer.parseInt(scanner.nextLine());
         int secondNum = Integer.parseInt(scanner.nextLine());
-        int result = 0;
 
 
         if (command.equals("add")) {
-            result = add(firstNum, secondNum);
+            add(firstNum,secondNum);
         } else if (command.equals("multiply")) {
-            result = mult(firstNum, secondNum);
+            mult(firstNum,secondNum);
         } else if (command.equals("subtract")) {
-            result = subs(firstNum, secondNum);
+            subs(firstNum,secondNum);
         } else if (command.equals("divide")) {
-            result = divide(firstNum, secondNum);
+            divide(firstNum,secondNum);
         }
-        System.out.println(result);
     }
 
-    public static int add(int a, int b) {
-//        System.out.println(a + b);
-        return (a + b);
+    public static void add(int a, int b) {
+        System.out.println(a + b);
     }
-
-    public static int mult(int a, int b) {
-//        System.out.println(a * b);
-        return (a * b);
+    public static void mult(int a, int b) {
+        System.out.println(a * b);
     }
-
-    public static int subs(int a, int b) {
-//        System.out.println(a * b);
-        return (a - b);
+    public static void subs(int a, int b) {
+        System.out.println(a - b);
     }
-
-    public static int divide(int a, int b) {
-//        System.out.println(a * b);
-        return (a / b);
+    public static void divide(int a, int b) {
+        System.out.println(a / b);
     }
 }
