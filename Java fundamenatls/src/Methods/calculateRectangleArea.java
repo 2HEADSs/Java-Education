f2b5fb997ac51fa9340d1ec7eb3234c21c88215e
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-10-08 13:57:42 +0300

    Create calculateRectangleArea.java

---
+++ Java fundamenatls/src/Methods/calculateRectangleArea.java
@@ -0,0 +1,17 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class calculateRectangleArea {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double width = Double.parseDouble(scanner.nextLine());
+        double height = Double.parseDouble(scanner.nextLine());
+        System.out.printf("%.1f", result(width, height));
+    }
+
+    public static double result(double w, double h) {
+        return w * h;
+    };
+
+};
