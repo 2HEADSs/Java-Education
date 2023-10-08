4b423989b272fcc15f66fcf83a2f824fa5294c13
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-10-08 13:42:56 +0300

    Create orders.java

---
+++ Java fundamenatls/src/Methods/orders.java
@@ -0,0 +1,11 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class orders {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
