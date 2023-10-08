7f34deae86af34412bc164f84dc85c65138eb29c
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-10-08 14:04:41 +0300

    Create mathPower.java

---
+++ Java fundamenatls/src/Methods/mathPower.java
@@ -0,0 +1,13 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class mathPower {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double firstNum = Double.parseDouble(scanner.nextLine());
+        double secondNum = Double.parseDouble(scanner.nextLine());
+
+    }
+
+}
