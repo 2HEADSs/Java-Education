b13f9a114b3c13f12d593edadac9cf934c4890a1
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-10-08 22:31:57 +0300

    Update sumAdjacentEqualNumbers.java

--- Java fundamenatls/src/Lists/sumAdjacentEqualNumbers.java
+++ Java fundamenatls/src/Lists/sumAdjacentEqualNumbers.java
@@ -1,9 +1,29 @@
 package Lists;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class sumAdjacentEqualNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<Double> numbers = nextLineOfDoubles(scanner);
+
+//        for (double number : numbers) {
+//            System.out.println(number);
+//        }
+
+
     }
+
+
+    private static List<Double> nextLineOfDoubles(Scanner scanner) {
+        List<Double> numbers = new ArrayList<>();
+        String lineOfNumbers = scanner.nextLine();
+        String[] numbersAsString = lineOfNumbers.split(" ");
+        for (String s : numbersAsString) {
+            numbers.add(Double.parseDouble(s));
+        }
+        return numbers;
+    };
 }
