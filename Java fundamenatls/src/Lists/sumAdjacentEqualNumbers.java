e9b7dae04cac907b16a520f54f63ca91889d982e
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-10-12 20:31:35 +0300

    Update sumAdjacentEqualNumbers.java

--- Java fundamenatls/src/Lists/sumAdjacentEqualNumbers.java
+++ Java fundamenatls/src/Lists/sumAdjacentEqualNumbers.java
@@ -7,26 +7,29 @@ import java.util.Scanner;
 public class sumAdjacentEqualNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<Double> numbers = nextLineOfDoubles(scanner);
+        List<Integer> numbers = nextLineOfDoubles(scanner);
 
-        List<Double> nextNumbers = sumAdjacentNumbers(numbers);
+        List<Integer> nextNumbers = sumAdjacentNumbers(numbers);
         while (nextNumbers.size() != numbers.size()) {
             numbers = nextNumbers;
             nextNumbers = sumAdjacentNumbers(numbers);
         }
 
     //TODO print as documents
-            System.out.println(numbers);
+
+        for(int i = 0; i < numbers.size(); i ++){
+            System.out.print(numbers.get(i)+ " ");
+        };
 
 
 
     }
 
-    private static List<Double> sumAdjacentNumbers(List<Double> numbers) {
-        List<Double> nextNums = new ArrayList<>();
+    private static List<Integer> sumAdjacentNumbers(List<Integer> numbers) {
+        List<Integer> nextNums = new ArrayList<>();
         int i = 0;
         while (i < numbers.size()) {
-            double first = numbers.get(i);
+            int first = numbers.get(i);
 //            double second = numbers.get(i + 1);
 
             if (i < numbers.size() - 1 && numbers.get(i).equals(numbers.get(i + 1)) ) {
@@ -41,15 +44,15 @@ public class sumAdjacentEqualNumbers {
     }
 
 
-    private static List<Double> nextLineOfDoubles(Scanner scanner) {
-        List<Double> numbers = new ArrayList<>();
+    private static List<Integer> nextLineOfDoubles(Scanner scanner) {
+        List<Integer> numbers = new ArrayList<>();
         String lineOfNumbers = scanner.nextLine();
         String[] numbersAsString = lineOfNumbers.split(" ");
         for (String s : numbersAsString) {
-            numbers.add(Double.parseDouble(s));
+            numbers.add(Integer.parseInt(s));
         }
         return numbers;
     }
 
-    ;
+
 }
