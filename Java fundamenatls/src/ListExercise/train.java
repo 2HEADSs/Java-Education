1e76544fa2c7ad296c8b89980a5d73977c3e0f5d
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-10-15 14:21:55 +0300

    Update train.java

--- Java fundamenatls/src/ListExercise/train.java
+++ Java fundamenatls/src/ListExercise/train.java
@@ -10,29 +10,29 @@ public class train {
         Scanner scanner = new Scanner(System.in);
 
         List<Integer> wagons = Arrays.stream(scanner.nextLine()
-                .split("\\s+")).map(Integer::parseInt)
+                        .split("\\s+")).map(Integer::parseInt)
                 .collect(Collectors.toList());
 
         int maxCapacity = Integer.parseInt(scanner.nextLine());
         String line = scanner.nextLine();
 
-        while(!line.equals("end")){
+        while (!line.equals("end")) {
             String[] data = line.split(" ");
-            if(data[0].equals("Add")){
+            if (data[0].equals("Add")) {
                 wagons.add(Integer.parseInt(data[1]));
             } else {
                 int passengers = Integer.parseInt(data[0]);
                 for (int i = 0; i < wagons.size(); i++) {
-                        if((wagons.get(i) + passengers) <= maxCapacity){
-                            wagons.set(i, wagons.get(i) + passengers);
-                            break;
-                        }
+                    if ((wagons.get(i) + passengers) <= maxCapacity) {
+                        wagons.set(i, wagons.get(i) + passengers);
+                        break;
+                    }
                 }
             }
 
             line = scanner.nextLine();
         }
-        System.out.println(wagons);
-
+//        System.out.println(wagons.toString().replaceAll("[\\[\\]\\,]",""));
+        wagons.forEach((el -> System.out.print(el + " ")));
     }
 }
