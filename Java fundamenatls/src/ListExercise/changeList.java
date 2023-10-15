23069bf53fc7c1388a511b8b16025a9739d41f26
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-10-15 15:30:23 +0300

    Update changeList.java

--- Java fundamenatls/src/ListExercise/changeList.java
+++ Java fundamenatls/src/ListExercise/changeList.java
@@ -19,26 +19,35 @@ public class changeList {
             String command = data[0];
             switch (command){
                 case "Delete":
-                    int elementForRemove = Integer.parseInt(data[1]);
-                    for (int i = 0; i < numbersAsList.size(); i++) {
-                        if(numbersAsList.get(i) == elementForRemove){
-                            numbersAsList.remove(i);
-                            i--;
-                        }
-                    }
+                    deleteMethod(numbersAsList, data);
                     break;
                 case "Insert":
-                    int indexToAdd = Integer.parseInt(data[2]);
-                    int elementToAdd = Integer.parseInt(data[1]);
-                    if(indexToAdd <= numbersAsList.size()){
-                        numbersAsList.add(indexToAdd, elementToAdd);
-                        break;
-                    }
+                    insertMethod(numbersAsList, data);
 
             }
             line = scanner.nextLine();
         }
         numbersAsList.forEach((el -> System.out.print(el + " ")));
 
+    }
+
+    private static void insertMethod(List<Integer> numbersAsList, String[] data) {
+        int indexToAdd = Integer.parseInt(data[2]);
+        int elementToAdd = Integer.parseInt(data[1]);
+        if(indexToAdd <= numbersAsList.size()){
+            numbersAsList.add(indexToAdd, elementToAdd);
+            return;
+        }
+    }
+
+    private static void deleteMethod(List<Integer> numbersAsList, String[] data) {
+        int elementForRemove = Integer.parseInt(data[1]);
+        for (int i = 0; i < numbersAsList.size(); i++) {
+            if(numbersAsList.get(i) == elementForRemove){
+                numbersAsList.remove(i);
+                i--;
+            }
+        }
+        return;
     }
 }
