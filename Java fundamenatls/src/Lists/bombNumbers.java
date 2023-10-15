48e391aadbbe14162919b3d38249717a8a1c8619
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-10-15 16:46:33 +0300

    Update bombNumbers.java

--- Java fundamenatls/src/Lists/bombNumbers.java
+++ Java fundamenatls/src/Lists/bombNumbers.java
@@ -1,7 +1,20 @@
 package Lists;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class bombNumbers {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine()
+                        .split("\\s+")).map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        String[] data = scanner.nextLine().split("\\s+");
+        int bomb = Integer.parseInt(data[0]);
+        int power = Integer.parseInt(data[1]);
     }
 }
