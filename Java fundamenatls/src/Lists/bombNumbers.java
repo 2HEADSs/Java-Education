52f65e1fdcd902d6d777ebd6f8c103ee6753e717
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-10-15 17:00:29 +0300

    Update bombNumbers.java

--- Java fundamenatls/src/Lists/bombNumbers.java
+++ Java fundamenatls/src/Lists/bombNumbers.java
@@ -16,5 +16,26 @@ public class bombNumbers {
         String[] data = scanner.nextLine().split("\\s+");
         int bomb = Integer.parseInt(data[0]);
         int power = Integer.parseInt(data[1]);
+
+        while (numbers.contains(bomb)) {
+            int bombIndex = numbers.indexOf(bomb);
+            int start = bombIndex - power;
+            int end = bombIndex + power;
+
+            if (start < 0) {
+                start = 0;
+            }
+            if (end >= numbers.size()) {
+                end = numbers.size() - 1;
+            }
+            for (int i = start; i <= end; i++) {
+                    numbers.remove(start);
+            }
+        }
+        int sum = 0;
+        for(Integer number: numbers){
+            sum+=number;
+        }
+        System.out.println(sum);
     }
 }
