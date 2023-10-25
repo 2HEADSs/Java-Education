8ea64a4cf433c1286a0ef31024d17683ad65d9fa
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-10-25 17:47:54 +0300

    Update softUniReception.java

--- Java fundamenatls/src/MidExam/softUniReception.java
+++ Java fundamenatls/src/MidExam/softUniReception.java
@@ -1,4 +1,9 @@
 package MidExam;
 
+import java.util.Scanner;
+
 public class softUniReception {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
 }
