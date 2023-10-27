a9186540d31a6c4020bc5ee80dd78d00a1ebdcda
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-10-27 15:47:21 +0300

    Update shootForTheWin.java

--- Java fundamenatls/src/MidExam/shootForTheWin.java
+++ Java fundamenatls/src/MidExam/shootForTheWin.java
@@ -3,6 +3,15 @@ package MidExam;
 import java.util.Scanner;
 
 public class shootForTheWin {
-    Scanner scanner = new Scanner(System.in);
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] target = getNextIntArray(scanner);
+    }
+
+    private static int[] getNextIntArray(Scanner scanner) {
+
+        
+        return nextIntArray(scanner);
+    }
+
 }
