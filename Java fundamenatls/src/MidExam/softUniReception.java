93716d2019fe96fee77fc7b91c222ca656c8c17e
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-10-27 15:11:53 +0300

    Update softUniReception.java

--- Java fundamenatls/src/MidExam/softUniReception.java
+++ Java fundamenatls/src/MidExam/softUniReception.java
@@ -4,6 +4,13 @@ import java.util.Scanner;
 
 public class softUniReception {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+        int employeeFirst = scanner.nextInt();
+        int employeeSecond = scanner.nextInt();
+        int employeeThird = scanner.nextInt();
+        int studentsPerHour = employeeFirst + employeeSecond + employeeThird;
+//        int studentsPerHour = scanner.nextInt() + scanner.nextInt() + scanner.nextInt();
+
     }
 }
