ecd6615ef023c6bf55e26316986140c8d45b035a
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-10-28 23:22:12 +0300

    Create computerStore.java

---
+++ Java fundamenatls/src/PersonalPractiseMidExam/computerStore.java
@@ -0,0 +1,42 @@
+package PersonalPractiseMidExam;
+
+import java.util.Scanner;
+
+public class computerStore {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+        double price = 0;
+        while (!command.equals("special")) {
+            if (command.equals("regular")) {
+                break;
+            }
+            double partPrice = Double.parseDouble(command);
+            if (partPrice < 0) {
+                System.out.println("Invalid price!");
+            } else {
+                price += partPrice;
+            }
+            command = scanner.nextLine();
+        }
+
+        if (price == 0) {
+            System.out.println("Invalid order!");
+        } else {
+            System.out.println("Congratulations you've just bought a new computer!");
+
+            System.out.printf("Price without taxes: %d$", price);
+            double taxes = price * 0.2;
+            System.out.printf("Taxes: %d$", price);
+            price += taxes;
+            
+            if (command.equals("special")) {
+                price *= 0.8;
+            }
+            System.out.printf("Price without taxes: %d$", price);
+
+
+        }
+
+    }
+}
