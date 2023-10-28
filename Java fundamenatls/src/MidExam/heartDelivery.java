838e6e26bdd23769c8c85d76bf9e44e247c67b6d
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-10-28 21:51:00 +0300

    Update heartDelivery.java

--- Java fundamenatls/src/MidExam/heartDelivery.java
+++ Java fundamenatls/src/MidExam/heartDelivery.java
@@ -5,11 +5,15 @@ import java.util.Scanner;
 public class heartDelivery {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] targets = getNextIntArray(scanner);
-        System.out.println(targets);
+        int[] houses  = getNextIntArray(scanner, "@");
+        String command = scanner.nextLine();
+
+        while(command.equals("Love!")){
+
+        }
     }
 
-    private static int[] getNextIntArray(Scanner scanner) {
+    private static int[] getNextIntArray(Scanner scanner, String separator) {
         String[] intAsStrings = scanner.nextLine().split("@");
         int[] array = new int[intAsStrings.length];
         for (int i = 0; i < array.length; i++) {
