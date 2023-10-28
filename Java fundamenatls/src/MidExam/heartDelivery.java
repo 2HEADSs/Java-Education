e619e2f34ee64d4a54a273256ce8143149e97633
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-10-28 21:57:05 +0300

    Update heartDelivery.java

--- Java fundamenatls/src/MidExam/heartDelivery.java
+++ Java fundamenatls/src/MidExam/heartDelivery.java
@@ -5,23 +5,31 @@ import java.util.Scanner;
 public class heartDelivery {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] houses  = getNextIntArray(scanner, "@");
+        int[] houses = getNextIntArray(scanner, "@");
         int currentPosition = 0;
         String command = scanner.nextLine();
 
-        while(!command.equals("Love!")){
+        while (!command.equals("Love!")) {
 
             command = scanner.nextLine();
         }
+
+
         int failed = 0;
         for (int i = 0; i < houses.length; i++) {
-            if(houses[i] !=0){
+            if (houses[i] != 0) {
                 failed++;
             }
         }
+
+
         System.out.printf("Cupid's last position was %d.%n", currentPosition);
-        System.out.printf("Cupid has failed %d places.%n", failed);
-        System.out.println("Mission was successful.");
+        if (failed > 0) {
+            System.out.printf("Cupid has failed %d places.%n", failed);
+        } else {
+            System.out.println("Mission was successful.");
+
+        }
     }
 
     private static int[] getNextIntArray(Scanner scanner, String separator) {
