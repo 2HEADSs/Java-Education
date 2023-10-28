71d39d441572a7caa7140e0e5ad4408f51410e94
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-10-28 21:37:27 +0300

    Create heartDelivery.java

---
+++ Java fundamenatls/src/MidExam/heartDelivery.java
@@ -0,0 +1,9 @@
+package MidExam;
+
+import java.util.Scanner;
+
+public class heartDelivery {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
