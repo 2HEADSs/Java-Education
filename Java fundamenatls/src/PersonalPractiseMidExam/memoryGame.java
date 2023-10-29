d2a8a97cc5b8294352977b20670cf87910011ac1
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-10-29 10:19:35 +0300

    Update memoryGame.java

--- Java fundamenatls/src/PersonalPractiseMidExam/memoryGame.java
+++ Java fundamenatls/src/PersonalPractiseMidExam/memoryGame.java
@@ -1,4 +1,9 @@
 package PersonalPractiseMidExam;
 
+import java.util.Scanner;
+
 public class memoryGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
 }
