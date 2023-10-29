87a7dfececa3feb432e1f6404f3d4d72ea2c1435
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-10-29 09:45:42 +0300

    Update theLift.java

--- Java fundamenatls/src/PersonalPractiseMidExam/theLift.java
+++ Java fundamenatls/src/PersonalPractiseMidExam/theLift.java
@@ -5,5 +5,16 @@ import java.util.Scanner;
 public class theLift {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int waitingPeople = scanner.nextInt();
+        int [] wagons = getNextIntArray(scanner, " ");
+    }
+
+    private static int[] getNextIntArray(Scanner scanner, String separator){
+        String[] intAsString = scanner.nextLine().split(separator);
+        int[] array = new int[intAsString.length];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Integer.parseInt(intAsString[i]);
+        }
+        return array;
     }
 }
