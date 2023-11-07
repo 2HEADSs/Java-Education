b3675885eb62f35d9e8c8af5955e4ecfbe4cb4f0
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-07 13:06:18 +0200

    Update Students.java

--- Java fundamenatls/src/ObjectsAndClasses/Students.java
+++ Java fundamenatls/src/ObjectsAndClasses/Students.java
@@ -5,5 +5,11 @@ import java.util.Scanner;
 public class Students {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        while(!line.equals("end")){
+
+            line = scanner.nextLine();
+        }
+        String city = scanner.nextLine();
     }
 }
