5f098c5540ff3ae2e541314122905c1c80597f83
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-07 13:11:48 +0200

    Update Students.java

--- Java fundamenatls/src/ObjectsAndClasses/Students.java
+++ Java fundamenatls/src/ObjectsAndClasses/Students.java
@@ -6,21 +6,27 @@ import java.util.Scanner;
 
 public class Students {
     static class Student {
+        boolean isFrom(String city){
 
+        }
+
+        String getInfo(){
+
+        }
     }
 
     public static void main(String[] args) {
         List<Student> students = new ArrayList<>();
-        students.add(new Student());
         Scanner scanner = new Scanner(System.in);
         String line = scanner.nextLine();
         while (!line.equals("end")) {
+            students.add(new Student());
 
             line = scanner.nextLine();
         }
         String city = scanner.nextLine();
         for(Student student: students){
-            if(student.getCity().equals(city)){
+            if(student.isFrom(city)){
                 System.out.println(student.getInfo());
             }
         }
