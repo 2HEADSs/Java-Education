102357d77bf912664cbb545b6016801d1ae5447f
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-07 14:00:15 +0200

    Create Students2.java

---
+++ Java fundamenatls/src/ObjectsAndClasses/Students2.java
@@ -0,0 +1,60 @@
+package ObjectsAndClasses;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Students2 {
+    static class Student {
+        String firstName;
+        String lastName;
+        int age;
+        String city;
+        Student(String firstName, String lastName, int age,String city){
+
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.city = city;
+        }
+        boolean isFrom(String city){
+            return  this.city.equals(city);
+        }
+
+        String getInfo(){
+            return  String.format("%s %s is %d years old",this.firstName,this.lastName, this.age);
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Student> students = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        while (!line.equals("end")) {
+            String [] studentParameters = line.split("\\s+");
+            String firstName = studentParameters[0];
+            String lastName = studentParameters[1];
+            int age = (Integer.parseInt(studentParameters[2]));
+            String city = studentParameters[3];
+
+            int index = indexOfStudent(students, firstName, lastName);
+            if(index == -1){
+                students.add(new Student(firstName,lastName,age,city));
+            }else {
+                students.set(index, new Student(firstName,lastName,age,city));
+
+            }
+
+
+
+
+            line = scanner.nextLine();
+        }
+        String city = scanner.nextLine();
+        for(Student student: students){
+            if(student.isFrom(city)){
+                System.out.println(student.getInfo());
+            }
+        }
+    }
+}
