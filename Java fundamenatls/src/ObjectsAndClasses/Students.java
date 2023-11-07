116a4d2f04e3ad3f9b377c6272bb9cf4a1c1741a
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-07 13:09:02 +0200

    Update Students.java

--- Java fundamenatls/src/ObjectsAndClasses/Students.java
+++ Java fundamenatls/src/ObjectsAndClasses/Students.java
@@ -1,15 +1,28 @@
 package ObjectsAndClasses;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Students {
+    static class Student {
+
+    }
+
     public static void main(String[] args) {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student());
         Scanner scanner = new Scanner(System.in);
         String line = scanner.nextLine();
-        while(!line.equals("end")){
+        while (!line.equals("end")) {
 
             line = scanner.nextLine();
         }
         String city = scanner.nextLine();
+        for(Student student: students){
+            if(student.getCity().equals(city)){
+                System.out.println(student.getInfo());
+            }
+        }
     }
 }
