57c4128af965b2e9d3dbc68b0193a6ef055d0072
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-07 12:25:35 +0200

    Update bigFactorial.java

--- Java fundamenatls/src/ObjectsAndClasses/bigFactorial.java
+++ Java fundamenatls/src/ObjectsAndClasses/bigFactorial.java
@@ -1,7 +1,16 @@
 package ObjectsAndClasses;
 
+import java.math.BigInteger;
+
 public class bigFactorial {
     public static void main(String[] args) {
-        
+        int n = 5;
+        BigInteger factorial = BigInteger.valueOf(1);
+
+        for (int i = 1; i <= n; i++) {
+            factorial = factorial.multiply(BigInteger.valueOf(i));
+        }
+
+        System.out.println(factorial);
     }
 }
