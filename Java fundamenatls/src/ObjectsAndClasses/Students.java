3325f3a4ccda7fbf1fd35c1b0d5b81961585cff7
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-07 13:51:54 +0200

    Update Students.java

--- Java fundamenatls/src/ObjectsAndClasses/Students.java
+++ Java fundamenatls/src/ObjectsAndClasses/Students.java
@@ -6,12 +6,24 @@ import java.util.Scanner;
 
 public class Students {
     static class Student {
-        boolean isFrom(String city){
+        String firstName;
+        String lastName;
+        int age;
+        private final String city;
+        String ciy;
+        Student(String firstName, String lastName, int age,String city){
 
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.city = city;
+        }
+        boolean isFrom(String city){
+            return  this.city.equals(city);
         }
 
         String getInfo(){
-
+            return  String.format("%s %s is %d years old.",this.firstName,this.lastName, this.age);
         }
     }
 
@@ -20,7 +32,15 @@ public class Students {
         Scanner scanner = new Scanner(System.in);
         String line = scanner.nextLine();
         while (!line.equals("end")) {
-            students.add(new Student());
+            String [] studentParameters = line.split("\\s+");
+            String firstName = studentParameters[0];
+            String lastName = studentParameters[1];
+            int age = (Integer.parseInt(studentParameters[2]);
+            String city = studentParameters[3];
+            students.add(new Student(firstName,lastName,age,city));
+
+
+
 
             line = scanner.nextLine();
         }
