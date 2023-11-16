bf01cda60526fefa19e4f2b3098bef61ebaa3608
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-16 16:26:28 +0200

    Update demo.java

--- Java fundamenatls/src/ObjectAndClassesExersice/demo.java
+++ Java fundamenatls/src/ObjectAndClassesExersice/demo.java
@@ -1,4 +1,26 @@
 package ObjectAndClassesExersice;
 
 public class demo {
+    public static void main(String[] args) {
+
+
+        Person p1 = new Person("pavel", 23);
+        Person p2 = new Person();
+
+    }
+
+    static class Person {
+        String name;
+        int year;
+
+        Person() {
+        }
+
+        ;
+
+        Person(String name, int year) {
+            this.name = name;
+            this.year = year;
+        }
+    }
 }
