39981640b1fdb4757ac3b6b1584a037b66b30b00
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-16 23:53:02 +0200

    Update Articles.java

--- Java fundamenatls/src/ObjectAndClassesExersice/Articles.java
+++ Java fundamenatls/src/ObjectAndClassesExersice/Articles.java
@@ -5,6 +5,28 @@ import java.util.Scanner;
 public class Articles {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] data = scanner.nextLine().split(", ");
+
+        String title = data[0];
+        String content = data[1];
+        String author = data[2];
+        Article article = new Article(title, content, author);
+        int n = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < n; i++) {
+            String [] command = scanner.nextLine().split(": ");
+            switch (command[0]){
+                case "Edit":
+                    article.setContent(command[1]);
+                    break;
+                    case "ChangeAuthor":
+                        article.setAuthor(command[1]);
+                    break;
+                    case "Rename":
+                        article.setTitle(command[1]);
+                    break;
+            }
+        }
+        System.out.println(article.toString());
     }
 
     static class Article {
@@ -45,7 +67,7 @@ public class Articles {
         @Override
         public String toString() {
 //            "{title} - {content}:{author}"
-            return String.format("%s - %s:%s%n", this.title, this.content, this.author);
+            return String.format("%s - %s: %s%n", this.title, this.content, this.author);
         }
     }
 
