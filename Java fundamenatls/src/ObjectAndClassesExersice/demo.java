4c78cc8293097a92673255930995947cc9ad3c4b
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-16 19:09:31 +0200

    Update demo.java

--- Java fundamenatls/src/ObjectAndClassesExersice/demo.java
+++ Java fundamenatls/src/ObjectAndClassesExersice/demo.java
@@ -4,9 +4,11 @@ public class demo {
     public static void main(String[] args) {
 
 
-        Person p1 = new Person("pavel", 23);
-        Person p2 = new Person();
+        Person p1 = new Person("Pavel", 31);
+        Person p2 = new Person("Peter", 34);
+        Person p3 = new Person("Ivan", 21);
 
+        
     }
 
     static class Person {
@@ -14,13 +16,14 @@ public class demo {
         int year;
 
         Person() {
-        }
-
-        ;
+        };
 
         Person(String name, int year) {
             this.name = name;
             this.year = year;
+        }
+        String getData(){
+            return String.format("I am %s, and I'm %d years old", this.name, this.year);
         }
     }
 }
