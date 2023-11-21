53e05554cba2c599104e4b586a91d24c2da1aa75
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-21 23:19:23 +0200

    Update countRealNumbers.java

--- Java fundamenatls/src/AssociativeArrays_Map/countRealNumbers.java
+++ Java fundamenatls/src/AssociativeArrays_Map/countRealNumbers.java
@@ -1,7 +1,22 @@
 package AssociativeArrays_Map;
 
+import java.lang.reflect.Array;
+import java.util.*;
+
 public class countRealNumbers {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        double[] numbers = Arrays.stream(scanner.nextLine()
+                        .split(" "))
+                .mapToDouble(Double::parseDouble).toArray();
+
+        Map<Double, Integer> numberCounts = new HashMap<>();
+        for (double number : numbers) {
+                
+        }
+
+
     }
+
 }
