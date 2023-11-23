91a43acb1096a423a71d55017deaab6abffe3fdc
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-23 23:17:29 +0200

    Create DemoTask.java

---
+++ Java fundamenatls/src/AssociativeArrays_Map/DemoTask.java
@@ -0,0 +1,13 @@
+package AssociativeArrays_Map;
+
+import java.util.*;
+
+public class DemoTask {
+    public static void main(String[] args) {
+        List<String> words = Arrays.asList("hello", "bye",
+                "the", "quick", "brown", "fox", "jumps",
+                "over", "the", "lazy", "dog");
+
+        Map<Integer, List<String>> wordsByLength = new TreeMap<>();
+    }
+}
