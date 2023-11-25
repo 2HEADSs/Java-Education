9b6431b9010f339425aa5cda20e094e6e3d2c488
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-25 18:38:03 +0200

    Create CountCharsInaString.java

---
+++ Java fundamenatls/src/AssociativeArrays_Map_Exercise/CountCharsInaString.java
@@ -0,0 +1,17 @@
+package AssociativeArrays_Map_Exercise;
+
+import java.util.Map;
+import java.util.Scanner;
+
+public class CountCharsInaString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] text = scanner.nextLine().split(" ");
+        for (String string : text) {
+            for (int i = 0; i < string.length(); i++) {
+                char symbol = string.charAt(i);
+            }
+        }
+//        Map<Character, Integer>
+    }
+}
