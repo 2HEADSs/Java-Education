06021fd59101151ca791b8043f17cc7e46297024
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-26 21:46:07 +0200

    Create MinerTask.java

---
+++ Java fundamenatls/src/AssociativeArrays_Map_Exercise/MinerTask.java
@@ -0,0 +1,10 @@
+package AssociativeArrays_Map_Exercise;
+
+import java.util.Scanner;
+
+public class MinerTask {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
