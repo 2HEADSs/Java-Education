25fe8d2983e15fcd21190c260da5f58ed58658e8
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-26 22:13:32 +0200

    Create LegendaryFarming.java

---
+++ Java fundamenatls/src/AssociativeArrays_Map_Exercise/LegendaryFarming.java
@@ -0,0 +1,16 @@
+package AssociativeArrays_Map_Exercise;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class LegendaryFarming {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Map<String, Integer> items = new HashMap<>();
+
+        Map<String, Integer> material = new HashMap<>();
+        
+    }
+}
