7848c1334355335857982e954e7318f1e587bf88
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-26 22:17:47 +0200

    Update LegendaryFarming.java

--- Java fundamenatls/src/AssociativeArrays_Map_Exercise/LegendaryFarming.java
+++ Java fundamenatls/src/AssociativeArrays_Map_Exercise/LegendaryFarming.java
@@ -7,10 +7,20 @@ import java.util.Scanner;
 public class LegendaryFarming {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         Map<String, Integer> items = new HashMap<>();
+        Map<String, Integer> materials = new HashMap<>();
 
-        Map<String, Integer> material = new HashMap<>();
-        
+        boolean hasWinner = false;
+        while (!hasWinner) {
+
+            String[] data = scanner.nextLine().split("\\s+");
+            for (int i = 0; i < data.length - 1; i++) {
+                int quantity = Integer.parseInt(data[i]);
+                String material = data[i + 1];
+                
+            }
+
+        }
+
     }
 }
