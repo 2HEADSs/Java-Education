d52daef3422e6b5202581f691162bd8c6cc8caa6
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-27 16:32:06 +0200

    Update Orders.java

--- Java fundamenatls/src/AssociativeArrays_Map_Exercise/Orders.java
+++ Java fundamenatls/src/AssociativeArrays_Map_Exercise/Orders.java
@@ -1,9 +1,28 @@
 package AssociativeArrays_Map_Exercise;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Orders {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        String line = scanner.nextLine();
+        Map<String, List<Double>> products = new LinkedHashMap<>();
+        while(!line.equals("buy")){
+            String[] data = line.split("\\s+");
+            String name = data[0];
+            double price = Double.parseDouble(data[1]);
+            double quantity = Double.parseDouble(data[2]);
+
+            if(products.containsKey(name)){
+                products.get(name).set(0,price);
+                products.get(name).set(1, products.get(name).get(1) + quantity);
+            }else {
+                products.put(name, new ArrayList<>());
+                products.get(name).add(0,price);
+                products.get(name).add(1,quantity);
+            }
+            line = scanner.nextLine();
+        }
     }
 }
