b5942ec4c9371d1edbdeabef4505ddcdb2b2cc31
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-27 16:07:22 +0200

    Create Orders.java

---
+++ Java fundamenatls/src/AssociativeArrays_Map_Exercise/Orders.java
@@ -0,0 +1,9 @@
+package AssociativeArrays_Map_Exercise;
+
+import java.util.Scanner;
+
+public class Orders {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
