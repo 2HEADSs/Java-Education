6a8632e9ad29d62c1103335750bc749228025754
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-27 16:34:30 +0200

    Update Orders.java

--- Java fundamenatls/src/AssociativeArrays_Map_Exercise/Orders.java
+++ Java fundamenatls/src/AssociativeArrays_Map_Exercise/Orders.java
@@ -8,21 +8,22 @@ public class Orders {
 
         String line = scanner.nextLine();
         Map<String, List<Double>> products = new LinkedHashMap<>();
-        while(!line.equals("buy")){
+        while (!line.equals("buy")) {
             String[] data = line.split("\\s+");
             String name = data[0];
             double price = Double.parseDouble(data[1]);
             double quantity = Double.parseDouble(data[2]);
 
-            if(products.containsKey(name)){
-                products.get(name).set(0,price);
+            if (products.containsKey(name)) {
+                products.get(name).set(0, price);
                 products.get(name).set(1, products.get(name).get(1) + quantity);
-            }else {
+            } else {
                 products.put(name, new ArrayList<>());
-                products.get(name).add(0,price);
-                products.get(name).add(1,quantity);
+                products.get(name).add(0, price);
+                products.get(name).add(1, quantity);
             }
             line = scanner.nextLine();
         }
+        products.forEach((k, v) -> System.out.printf("%s -> %.2f%n", k, v.get(0) * v.get(1)));
     }
 }
