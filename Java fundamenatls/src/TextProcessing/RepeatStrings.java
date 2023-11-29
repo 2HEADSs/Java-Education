24229171b305f3fddf2944c800d60a865e8b5ee9
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-29 10:46:21 +0200

    Update RepeatStrings.java

--- Java fundamenatls/src/TextProcessing/RepeatStrings.java
+++ Java fundamenatls/src/TextProcessing/RepeatStrings.java
@@ -5,16 +5,19 @@ import java.util.Scanner;
 public class RepeatStrings {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String [] words = scanner.nextLine().split("\\s+");
+        String[] words = scanner.nextLine().split("\\s+");
         for (String word : words) {
-            System.out.print(repeat(word, words.length));
+            System.out.print(repeat(word, word.length()));
         }
 
     }
 
     private static String repeat(String word, int repetition) {
-        char [] repeated = new char[word.length() * repetition];
+        char[] repeated = new char[word.length() * repetition];
 
+        for (int i = 0; i < repeated.length; i++) {
+            repeated[i] = word.charAt(i % word.length());
+        }
 
         return new String(repeated);
     }
