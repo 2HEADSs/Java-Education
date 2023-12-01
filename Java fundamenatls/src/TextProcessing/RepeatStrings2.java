259aab77af5cd3af6fd3bf1191250ce7c62ad013
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-12-01 15:22:24 +0200

    Create RepeatStrings2.java

---
+++ Java fundamenatls/src/TextProcessing/RepeatStrings2.java
@@ -0,0 +1,24 @@
+package TextProcessing;
+
+import java.util.Scanner;
+
+public class RepeatStrings2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] words = scanner.nextLine().split("\\s+");
+        for (String word : words) {
+            System.out.print(repeat(word, word.length()));
+        }
+
+    }
+
+    static String repeat(String word, int repetition) {
+        char[] repeated = new char[word.length() * repetition];
+
+        for (int i = 0; i < repeated.length; i++) {
+            repeated[i] = word.charAt(i % word.length());
+        }
+
+        return new String(repeated);
+    }
+}
