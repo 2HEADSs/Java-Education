81196b51d4155e7aa5a122358eee4762517db465
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-12-04 22:37:20 +0200

    Update TheImitationGame.java

--- Java fundamenatls/src/FundFinalExamPrep/TheImitationGame.java
+++ Java fundamenatls/src/FundFinalExamPrep/TheImitationGame.java
@@ -5,5 +5,14 @@ import java.util.Scanner;
 public class TheImitationGame {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String message = scanner.nextLine();
+        String commands = scanner.nextLine();
+
+        while (!commands.equals("Decode")) {
+
+
+            commands = scanner.nextLine();
+        }
+
     }
 }
