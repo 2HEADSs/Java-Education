2ae647a8a64fdb5612446f0b5f044d7e8a2daae5
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-12-04 18:14:35 +0200

    Create TheImitationGame.java

---
+++ Java fundamenatls/src/FundFinalExamPrep/TheImitationGame.java
@@ -0,0 +1,9 @@
+package FundFinalExamPrep;
+
+import java.util.Scanner;
+
+public class TheImitationGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
