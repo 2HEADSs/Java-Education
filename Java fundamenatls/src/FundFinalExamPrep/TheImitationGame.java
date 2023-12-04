98fe499e37273486eff611e4939256332c517085
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-12-04 22:48:17 +0200

    Update TheImitationGame.java

--- Java fundamenatls/src/FundFinalExamPrep/TheImitationGame.java
+++ Java fundamenatls/src/FundFinalExamPrep/TheImitationGame.java
@@ -9,10 +9,27 @@ public class TheImitationGame {
         String commands = scanner.nextLine();
 
         while (!commands.equals("Decode")) {
+            String[] commandParts = commands.split("\\|");
+            String commandName = commandParts[0];
+            switch (commandName) {
+                case "Move":
+
+                    break;
+                case "Insert":
+
+                    break;
+                case "ChangeAll":
+
+                    break;
+                default:
+                    throw  new IllegalStateException("Unknown command " + commandName);
+            }
 
 
             commands = scanner.nextLine();
         }
+
+        System.out.println("The decrypted message is: " + message);
 
     }
 }
